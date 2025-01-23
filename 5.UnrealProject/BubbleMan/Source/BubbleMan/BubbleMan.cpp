b9868c6625828ee95a7e7faefd9a862fcc5da674
Alijimpa <alibadpa007@gmail.com>
2025-01-23 12:01:10 +0330

    ChangeProject to Blueprint

--- 5.UnrealProject/BubbleMan/Source/BubbleMan/BubbleMan.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BubbleMan.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BubbleMan, "BubbleMan" );
